7fc4066fa49480b9c8584cf0a23ae9b0093873a2
Zameer Ansari <zn.ansari21@gmail.com>
2020-02-10 18:59:19 -0500

    initial commit

---
+++ main.c
@@ -0,0 +1,203 @@
+/**
+  Generated Main Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    main.c
+
+  Summary:
+    This is the main file generated using PIC10 / PIC12 / PIC16 / PIC18 MCUs
+
+  Description:
+    This header file provides implementations for driver APIs for all modules selected in the GUI.
+    Generation Information :
+        Product Revision  :  PIC10 / PIC12 / PIC16 / PIC18 MCUs - 1.78
+        Device            :  PIC16F18346
+        Driver Version    :  2.00
+ */
+
+/*
+    (c) 2018 Microchip Technology Inc. and its subsidiaries. 
+    
+    Subject to your compliance with these terms, you may use Microchip software and any 
+    derivatives exclusively with Microchip products. It is your responsibility to comply with third party 
+    license terms applicable to your use of third party software (including open source software) that 
+    may accompany Microchip software.
+    
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER 
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY 
+    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS 
+    FOR A PARTICULAR PURPOSE.
+    
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP 
+    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO 
+    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL 
+    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT 
+    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS 
+    SOFTWARE.
+ */
+
+#include "mcc_generated_files/mcc.h"
+#include "mcc_generated_files/examples/i2c1_master_example.h"
+/*
+ Constant Declarations
+ */
+#define DELAY 13.3333333333
+#define PRSR_SNSR_ADDR 0x5D
+#define PRSR_H 0x2A
+#define PRSR_M 0x29
+#define PRSR_L 0x28
+#define TMPR_H 0x2C
+#define TMPR_L 0x2B
+/*
+ Global Variables
+ */
+uint32_t raw_pressure;
+uint16_t raw_temperature;
+uint8_t value;
+static float pressure;
+static float temperature;
+
+
+/*
+ 
+ * Subroutines
+ 
+ */
+void setHigh() {
+    //set PWM frequency to 9.52kHz
+    PWM5CON = 0x80;
+    PR2 = 0x68;
+    PWM5DCH = 0x34;
+
+}
+
+void setLow() {
+    //set PWM frequency to 8.54kHz
+    PWM5CON = 0x80;
+    PR2 = 0x74;
+    PWM5DCH = 0x3A;
+}
+
+void setZero() {
+    //shutdown PWM
+    PWM5CON = 0x00;
+}
+
+bool sendPWM(uint8_t *data) {
+    uint8_t j = 0;
+    //creating a PWM wave in an UART fashion
+    setLow();
+    __delay_ms(DELAY);
+    //    for (i = 0; i < n; i++) { //send_pwm(&dummy_data[i]); 
+    for (j = 0; j < 8; j++) {
+        (!!((*data >> j)&0x01)) ? setHigh() : setLow();
+        __delay_ms(DELAY);
+    }
+    //    }
+    setHigh();
+    __delay_ms(DELAY);
+    if (*data == 0)return true;
+    return false;
+    ////////////////////////////////////////////////////////////////////
+}
+
+void readPressureSensor() {
+    //read pressure registers
+    raw_pressure = 0;
+    value = 0;
+    value = I2C1_Read1ByteRegister(PRSR_SNSR_ADDR, 0x2A);
+    raw_pressure = value;
+    value = I2C1_Read1ByteRegister(PRSR_SNSR_ADDR, 0x29);
+    raw_pressure = (raw_pressure << 8) + value;
+    value = I2C1_Read1ByteRegister(PRSR_SNSR_ADDR, 0x28);
+    raw_pressure = (raw_pressure << 8) + value;
+    if (raw_pressure & 0x800000) {
+        raw_pressure = (0xff000000 | raw_pressure);
+    }
+    pressure = (float) (raw_pressure) / 4096.0;
+    __delay_ms(100);
+    //read temperature registers
+    raw_temperature = 0;
+    value = 0;
+    value = I2C1_Read1ByteRegister(PRSR_SNSR_ADDR, TMPR_H);
+    raw_temperature = value;
+    value = I2C1_Read1ByteRegister(PRSR_SNSR_ADDR, TMPR_L);
+    raw_temperature = (raw_temperature << 8) + value;
+    temperature = (float) (raw_temperature) / 100.00;
+    
+//            printf("Pressure : %f\n",pressure);
+//            printf("Temperature : %f\n",temperature);
+}
+void sendFloat(float * f){
+    unsigned char **temp = (unsigned char**) &f;
+    sendPWM(*temp++);
+    sendPWM(*temp++);
+    sendPWM(*temp++);
+    sendPWM(*temp);
+}
+/*
+                         Main application
+ */
+void main(void) {
+    //initialize the variables
+    char hdr_trl[] = {0xFF, 0x00, 0xFF, 0x00};
+    // initialize the device
+    SYSTEM_Initialize();    
+    setZero();         //shutdown the PWM
+    pressure = 1013.134;
+    temperature = 24.76;
+    // When using interrupts, you need to set the Global and Peripheral Interrupt Enable bits
+    // Use the following macros to:
+
+    // Enable the Global Interrupts
+    INTERRUPT_GlobalInterruptEnable();
+
+    // Enable the Peripheral Interrupts
+    INTERRUPT_PeripheralInterruptEnable();
+
+    // Disable the Global Interrupts
+    //INTERRUPT_GlobalInterruptDisable();
+
+    // Disable the Peripheral Interrupts
+    //INTERRUPT_PeripheralInterruptDisable();
+    char dummy_data[] = {0xFF, 0x00, 0xFF, 0x00, 0x16, 'H', 'E', 'L', 'L', 'O', ' ', 'S', 'E', 'N', 'S', 'O', 'R', 'S', 0x17, 0xFF, 0x00, 0xFF, 0x00};
+    uint8_t n = sizeof (dummy_data);
+    uint8_t i = 0;
+    while (1) {
+        // Add your application code
+        readPressureSensor();
+        // routine to measure pressure and sensor
+//        readPsrSnsr();
+        
+        /*The whole sending data sequence through PWM and UART*/
+        //keep the PWM on PORTC in high input impedance state
+//        TRISC = 0xFF;
+//        if (PORTCbits.RC4 == 1) {
+//        }//check if button is pressed and start sending
+//        else {
+//            LATCbits.LATC3 = 1; //turn ON LED to indicate transmission
+//            TRISC = 0xBF; //set the PORT C PWM to o/p
+////            for (i = 0; i < n; i++) {
+////                sendPWM(&dummy_data[i]);
+////            }
+//            //start header pwm
+////            for(i=0;i<4;i++)sendPWM(&hdr_trl[i]);
+//            readPsrSnsr();
+////            sendFloat(&pressure);
+////            sendFloat(&temperature);                    
+//            //end trailer pwm
+////            for(i=0;i<4;i++)sendPWM(&hdr_trl[i]);
+//            //            send_pwm(&i); //make data and send one byte at a time.//sending dummy data now
+//            LATCbits.LATC3 = 0; //turn OFF LED to indicate end
+//            //            printf("%c", 'd'); //send data to UART to test at 1200 baud
+//        }
+    }
+}
+/**
+ End of File
+ */
---
+++ mcc_generated_files/pin_manager.h
@@ -0,0 +1,213 @@
+/**
+  @Generated Pin Manager Header File
+
+  @Company:
+    Microchip Technology Inc.
+
+  @File Name:
+    pin_manager.h
+
+  @Summary:
+    This is the Pin Manager file generated using PIC10 / PIC12 / PIC16 / PIC18 MCUs
+
+  @Description
+    This header file provides APIs for driver for .
+    Generation Information :
+        Product Revision  :  PIC10 / PIC12 / PIC16 / PIC18 MCUs - 1.78
+        Device            :  PIC16F18346
+        Driver Version    :  2.11
+    The generated drivers are tested against the following:
+        Compiler          :  XC8 2.05 and above
+        MPLAB 	          :  MPLAB X 5.20	
+*/
+
+/*
+    (c) 2018 Microchip Technology Inc. and its subsidiaries. 
+    
+    Subject to your compliance with these terms, you may use Microchip software and any 
+    derivatives exclusively with Microchip products. It is your responsibility to comply with third party 
+    license terms applicable to your use of third party software (including open source software) that 
+    may accompany Microchip software.
+    
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER 
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY 
+    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS 
+    FOR A PARTICULAR PURPOSE.
+    
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP 
+    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO 
+    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL 
+    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT 
+    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS 
+    SOFTWARE.
+*/
+
+#ifndef PIN_MANAGER_H
+#define PIN_MANAGER_H
+
+/**
+  Section: Included Files
+*/
+
+#include <xc.h>
+
+#define INPUT   1
+#define OUTPUT  0
+
+#define HIGH    1
+#define LOW     0
+
+#define ANALOG      1
+#define DIGITAL     0
+
+#define PULL_UP_ENABLED      1
+#define PULL_UP_DISABLED     0
+
+// get/set SDA1 aliases
+#define SDA1_TRIS                 TRISBbits.TRISB4
+#define SDA1_LAT                  LATBbits.LATB4
+#define SDA1_PORT                 PORTBbits.RB4
+#define SDA1_WPU                  WPUBbits.WPUB4
+#define SDA1_OD                   ODCONBbits.ODCB4
+#define SDA1_ANS                  ANSELBbits.ANSB4
+#define SDA1_SetHigh()            do { LATBbits.LATB4 = 1; } while(0)
+#define SDA1_SetLow()             do { LATBbits.LATB4 = 0; } while(0)
+#define SDA1_Toggle()             do { LATBbits.LATB4 = ~LATBbits.LATB4; } while(0)
+#define SDA1_GetValue()           PORTBbits.RB4
+#define SDA1_SetDigitalInput()    do { TRISBbits.TRISB4 = 1; } while(0)
+#define SDA1_SetDigitalOutput()   do { TRISBbits.TRISB4 = 0; } while(0)
+#define SDA1_SetPullup()          do { WPUBbits.WPUB4 = 1; } while(0)
+#define SDA1_ResetPullup()        do { WPUBbits.WPUB4 = 0; } while(0)
+#define SDA1_SetPushPull()        do { ODCONBbits.ODCB4 = 0; } while(0)
+#define SDA1_SetOpenDrain()       do { ODCONBbits.ODCB4 = 1; } while(0)
+#define SDA1_SetAnalogMode()      do { ANSELBbits.ANSB4 = 1; } while(0)
+#define SDA1_SetDigitalMode()     do { ANSELBbits.ANSB4 = 0; } while(0)
+
+// get/set SCL1 aliases
+#define SCL1_TRIS                 TRISBbits.TRISB5
+#define SCL1_LAT                  LATBbits.LATB5
+#define SCL1_PORT                 PORTBbits.RB5
+#define SCL1_WPU                  WPUBbits.WPUB5
+#define SCL1_OD                   ODCONBbits.ODCB5
+#define SCL1_ANS                  ANSELBbits.ANSB5
+#define SCL1_SetHigh()            do { LATBbits.LATB5 = 1; } while(0)
+#define SCL1_SetLow()             do { LATBbits.LATB5 = 0; } while(0)
+#define SCL1_Toggle()             do { LATBbits.LATB5 = ~LATBbits.LATB5; } while(0)
+#define SCL1_GetValue()           PORTBbits.RB5
+#define SCL1_SetDigitalInput()    do { TRISBbits.TRISB5 = 1; } while(0)
+#define SCL1_SetDigitalOutput()   do { TRISBbits.TRISB5 = 0; } while(0)
+#define SCL1_SetPullup()          do { WPUBbits.WPUB5 = 1; } while(0)
+#define SCL1_ResetPullup()        do { WPUBbits.WPUB5 = 0; } while(0)
+#define SCL1_SetPushPull()        do { ODCONBbits.ODCB5 = 0; } while(0)
+#define SCL1_SetOpenDrain()       do { ODCONBbits.ODCB5 = 1; } while(0)
+#define SCL1_SetAnalogMode()      do { ANSELBbits.ANSB5 = 1; } while(0)
+#define SCL1_SetDigitalMode()     do { ANSELBbits.ANSB5 = 0; } while(0)
+
+// get/set RB6 procedures
+#define RB6_SetHigh()            do { LATBbits.LATB6 = 1; } while(0)
+#define RB6_SetLow()             do { LATBbits.LATB6 = 0; } while(0)
+#define RB6_Toggle()             do { LATBbits.LATB6 = ~LATBbits.LATB6; } while(0)
+#define RB6_GetValue()              PORTBbits.RB6
+#define RB6_SetDigitalInput()    do { TRISBbits.TRISB6 = 1; } while(0)
+#define RB6_SetDigitalOutput()   do { TRISBbits.TRISB6 = 0; } while(0)
+#define RB6_SetPullup()             do { WPUBbits.WPUB6 = 1; } while(0)
+#define RB6_ResetPullup()           do { WPUBbits.WPUB6 = 0; } while(0)
+#define RB6_SetAnalogMode()         do { ANSELBbits.ANSB6 = 1; } while(0)
+#define RB6_SetDigitalMode()        do { ANSELBbits.ANSB6 = 0; } while(0)
+
+// get/set RB7 procedures
+#define RB7_SetHigh()            do { LATBbits.LATB7 = 1; } while(0)
+#define RB7_SetLow()             do { LATBbits.LATB7 = 0; } while(0)
+#define RB7_Toggle()             do { LATBbits.LATB7 = ~LATBbits.LATB7; } while(0)
+#define RB7_GetValue()              PORTBbits.RB7
+#define RB7_SetDigitalInput()    do { TRISBbits.TRISB7 = 1; } while(0)
+#define RB7_SetDigitalOutput()   do { TRISBbits.TRISB7 = 0; } while(0)
+#define RB7_SetPullup()             do { WPUBbits.WPUB7 = 1; } while(0)
+#define RB7_ResetPullup()           do { WPUBbits.WPUB7 = 0; } while(0)
+#define RB7_SetAnalogMode()         do { ANSELBbits.ANSB7 = 1; } while(0)
+#define RB7_SetDigitalMode()        do { ANSELBbits.ANSB7 = 0; } while(0)
+
+// get/set LED aliases
+#define LED_TRIS                 TRISCbits.TRISC3
+#define LED_LAT                  LATCbits.LATC3
+#define LED_PORT                 PORTCbits.RC3
+#define LED_WPU                  WPUCbits.WPUC3
+#define LED_OD                   ODCONCbits.ODCC3
+#define LED_ANS                  ANSELCbits.ANSC3
+#define LED_SetHigh()            do { LATCbits.LATC3 = 1; } while(0)
+#define LED_SetLow()             do { LATCbits.LATC3 = 0; } while(0)
+#define LED_Toggle()             do { LATCbits.LATC3 = ~LATCbits.LATC3; } while(0)
+#define LED_GetValue()           PORTCbits.RC3
+#define LED_SetDigitalInput()    do { TRISCbits.TRISC3 = 1; } while(0)
+#define LED_SetDigitalOutput()   do { TRISCbits.TRISC3 = 0; } while(0)
+#define LED_SetPullup()          do { WPUCbits.WPUC3 = 1; } while(0)
+#define LED_ResetPullup()        do { WPUCbits.WPUC3 = 0; } while(0)
+#define LED_SetPushPull()        do { ODCONCbits.ODCC3 = 0; } while(0)
+#define LED_SetOpenDrain()       do { ODCONCbits.ODCC3 = 1; } while(0)
+#define LED_SetAnalogMode()      do { ANSELCbits.ANSC3 = 1; } while(0)
+#define LED_SetDigitalMode()     do { ANSELCbits.ANSC3 = 0; } while(0)
+
+// get/set SWITCH aliases
+#define SWITCH_TRIS                 TRISCbits.TRISC4
+#define SWITCH_LAT                  LATCbits.LATC4
+#define SWITCH_PORT                 PORTCbits.RC4
+#define SWITCH_WPU                  WPUCbits.WPUC4
+#define SWITCH_OD                   ODCONCbits.ODCC4
+#define SWITCH_ANS                  ANSELCbits.ANSC4
+#define SWITCH_SetHigh()            do { LATCbits.LATC4 = 1; } while(0)
+#define SWITCH_SetLow()             do { LATCbits.LATC4 = 0; } while(0)
+#define SWITCH_Toggle()             do { LATCbits.LATC4 = ~LATCbits.LATC4; } while(0)
+#define SWITCH_GetValue()           PORTCbits.RC4
+#define SWITCH_SetDigitalInput()    do { TRISCbits.TRISC4 = 1; } while(0)
+#define SWITCH_SetDigitalOutput()   do { TRISCbits.TRISC4 = 0; } while(0)
+#define SWITCH_SetPullup()          do { WPUCbits.WPUC4 = 1; } while(0)
+#define SWITCH_ResetPullup()        do { WPUCbits.WPUC4 = 0; } while(0)
+#define SWITCH_SetPushPull()        do { ODCONCbits.ODCC4 = 0; } while(0)
+#define SWITCH_SetOpenDrain()       do { ODCONCbits.ODCC4 = 1; } while(0)
+#define SWITCH_SetAnalogMode()      do { ANSELCbits.ANSC4 = 1; } while(0)
+#define SWITCH_SetDigitalMode()     do { ANSELCbits.ANSC4 = 0; } while(0)
+
+// get/set RC6 procedures
+#define RC6_SetHigh()            do { LATCbits.LATC6 = 1; } while(0)
+#define RC6_SetLow()             do { LATCbits.LATC6 = 0; } while(0)
+#define RC6_Toggle()             do { LATCbits.LATC6 = ~LATCbits.LATC6; } while(0)
+#define RC6_GetValue()              PORTCbits.RC6
+#define RC6_SetDigitalInput()    do { TRISCbits.TRISC6 = 1; } while(0)
+#define RC6_SetDigitalOutput()   do { TRISCbits.TRISC6 = 0; } while(0)
+#define RC6_SetPullup()             do { WPUCbits.WPUC6 = 1; } while(0)
+#define RC6_ResetPullup()           do { WPUCbits.WPUC6 = 0; } while(0)
+#define RC6_SetAnalogMode()         do { ANSELCbits.ANSC6 = 1; } while(0)
+#define RC6_SetDigitalMode()        do { ANSELCbits.ANSC6 = 0; } while(0)
+
+/**
+   @Param
+    none
+   @Returns
+    none
+   @Description
+    GPIO and peripheral I/O initialization
+   @Example
+    PIN_MANAGER_Initialize();
+ */
+void PIN_MANAGER_Initialize (void);
+
+/**
+ * @Param
+    none
+ * @Returns
+    none
+ * @Description
+    Interrupt on Change Handling routine
+ * @Example
+    PIN_MANAGER_IOC();
+ */
+void PIN_MANAGER_IOC(void);
+
+
+
+#endif // PIN_MANAGER_H
+/**
+ End of File
+*/
